3d43ba5867bd075740fc0eb83663a6d7d73d95d2
SungSoo Park <whiteibescu@gmail.com>
2021-05-25 05:41:22 +0900

    AddTwoNumbers

---
+++ AddTwoNumbers_CSharp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class Solution
+{
+    static void Main(string[] args)
+    {
+        ListNode l1 = new ListNode();
+        ListNode l2 = new ListNode();
+        AddTwoNumbers(l1, l2);
+    }
+    public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+    {
+        l1.val = 2;
+        l1.next = (ListNode)4;
+        l1.next = (ListNode)3;
+
+        foreach(var i in l1)
+        {
+            Console.WriteLine(i);
+        }
+
+        
+
+
+        return null;
+    }
+}
