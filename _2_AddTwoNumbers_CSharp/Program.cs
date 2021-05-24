75ca83537d86f91e8e88325f5d8f745c99d41fac
SungSoo Park <whiteibescu@gmail.com>
2021-05-24 15:33:01 +0900

    Change Name

---
+++ _2_AddTwoNumbers_CSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+/*  You are given two non-empty linked lists representing two non-negative integers.
+ *  The digits are sorted in reverse order, and each of their nodes contains a single digit.
+ *  Add the two numbers and return the sum as a linked list. You may assume the two
+ *  numbers do not contain any leading zero, except the number 0 itself.
+ * 
+ * 
+ */
+
+
+
+
+
+
+
+namespace AddTwoNumbers_CSharp
+{
+
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null) //constructor
+        {
+            this.val = val;  
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] l1 = { 2, 4, 3 };
+            int[] l2 = { 5, 6, 4 };
+
+
+        }
+
+    }
+}
